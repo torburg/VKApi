3a5eda3a808e80e15c18638b680ef9524469f538
torburg <torburg@yandex.ru>
2020-07-31 13:40:53 +0300

    Return LaunchScreen to Full Screen UIWindow

--- VKApi/MainListViewController.swift
+++ VKApi/MainListViewController.swift
@@ -9,22 +9,41 @@
 import UIKit
 
 class MainListViewController: UIViewController {
+    
+    var postList: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setPostListtable()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setPostListtable() {
+        let frame = CGRect(origin: view.center, size: view.frame.size)
+        postList = UITableView(frame: frame)
+        postList.backgroundColor = .white
+        postList.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(postList)
+        let constraints = [
+            postList.topAnchor.constraint(equalTo: view.topAnchor),
+            postList.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            postList.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            postList.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ]
+        NSLayoutConstraint.activate(constraints)
     }
-    */
 
+}
+
+extension MainListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = postList.dequeueReusableCell(withIdentifier: "post", for: indexPath)
+        return cell
+    }
+    
+    
 }
